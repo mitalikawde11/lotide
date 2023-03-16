9eac9d699abbdcec163ec7147c890be9addb2490
Mitali <mitali.kawde11@gmail.com>
2023-03-16 13:56:21 -0700

    test cases for head function in file are removed and moved to the new test file in test folder. added a require logic

---
+++ test/headTest.js
@@ -0,0 +1,9 @@
+// require(import) asserEqual and head file
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+// Test cases
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["John"]), "John");
+assertEqual(head([]), undefined);
