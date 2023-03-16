86487c5b59413589ac5a7e55255c1cc754fd0538
Mitali <mitali.kawde11@gmail.com>
2023-03-16 15:01:50 -0700

    Refactoring code: added eqArray require logic, exports assertArraysEqual function and moved test cases into test file(in test folder)

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if(arr1.length !== arr2.length) {
-    return false;
-  }
-  for(let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected) === true) {
@@ -18,7 +8,6 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3],[1, 2, 3]);
-assertArraysEqual([1, 2, 3],[1, 2, "3"]);
-assertArraysEqual([4, 5],[4, 5, null]);
-assertArraysEqual([1, 2, 3],[1, 2]);
+
+
+module.exports = assertArraysEqual;
