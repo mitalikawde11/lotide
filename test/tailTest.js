021b819170c987c2e3e629126a1b9230d5d7e92d
Mitali <mitali.kawde11@gmail.com>
2023-03-16 14:20:26 -0700

    moved the test cases of tail.js file to this file. require logic for tail and assert function

---
+++ test/tailTest.js
@@ -0,0 +1,16 @@
+// require(import) asserEqual and tail file
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+const names = ["Sam", "2"];
+tail(names);
+assertEqual(names.length, 2);
+
+const abc = [];
+tail(abc);
+assertEqual(abc.length, 0);
