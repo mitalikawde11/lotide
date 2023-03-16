7452515009a1470a01196017de5cb2894906e568
Mitali <mitali.kawde11@gmail.com>
2023-03-16 13:51:34 -0700

    Refactor the code. Now file only contain function definition and module.export and require logic

--- head.js
+++ head.js
@@ -1,14 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// head function should return the first item in the array.
+// this should not return the first element as an array, simply return the element itself.
+
+const assertEqual = require('./assertEqual');
+
 const head = function(arr) {
   return arr[0];
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["John"]), "John");
-assertEqual(head([]), 7);
+
+
+module.exports = head;
