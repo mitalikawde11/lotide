2679cc7be022abc98af447441f17067c9f2b18fc
Mitali <mitali.kawde11@gmail.com>
2023-03-16 23:36:35 -0700

    Applying the new, shorter, property value shorthand syntax to the objectcreated and exported in index.js file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Exporting object
+
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
@@ -18,21 +20,21 @@ const without = require('./without');
 
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual, 
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+  head,
+  tail,
+  middle,
+  assertArraysEqual, 
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
